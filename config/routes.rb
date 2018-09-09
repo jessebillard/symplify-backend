ce4879e237135865cebe9a25c5333aa266d077da
jessebillard <jessebillard@mac.com>
2018-09-09 15:20:46 -0400

    adds user_id to notes table, fixes syntax error on routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   
-  namespace api: do
-    namespace v1: do
-      resources :notes, only: [:create, :update, :destroy]
+  namespace :api do
+    namespace :v1 do
+      resources :notes, only: [:index, :create, :update, :destroy]
       resources :users, only: [:create, :show]
     end
   end
