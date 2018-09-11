725952edfe720d600bf11626a60e48a86a0d5758
jessebillard <jessebillard@mac.com>
2018-09-10 23:20:01 -0400

    configures cors for destroy method, builds destroy note method in controller

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::NotesController < ApplicationController
 
+    def index
+        @notes = Note.all
+        render json: @notes
+    end
+
     def create
 
     end
@@ -9,7 +14,20 @@ class Api::V1::NotesController < ApplicationController
     end
 
     def destroy
+        # byebug
+        @note = Note.find(params[:id])    
+        if @note
+            @note.destroy            
+            render json: {id: params[:id]}
+        else
+            render json: error_message
+        end
+    end
 
+    private
+
+    def error_message
+        {status: "error", code: 400, message: "This request cannot be completed"}
     end
 
 end
--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module SymplifyBackend
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :patch, :options]
+        resource '*', headers: :any, methods: [:get, :post, :patch, :options, :delete]
       end
     end
 
